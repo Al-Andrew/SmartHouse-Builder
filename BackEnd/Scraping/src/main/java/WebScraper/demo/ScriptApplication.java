3411f04ef3a966ae46510c81e079477c39647eb1
Aldea Andrei <aldea.andrei.977@gmail.com>
2022-04-20 23:46:32 +0300

    merged the two backend folters accidentally introduced

--- BackEnd/Scraping/src/main/java/WebScraper/demo/ScriptApplication.java
+++ BackEnd/Scraping/src/main/java/WebScraper/demo/ScriptApplication.java
@@ -7,6 +7,7 @@ import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ScriptApplication {
 
