d1fd0da05d429d6ea6c3af6fe9cf21087ef07b29
EnascutaRazvan <enascutar@gmail.com>
2022-04-20 22:07:16 +0300

    BackEnd -> Scraper

---
+++ BackEnd/Scraping/src/test/java/WebScraper/demo/ScriptApplicationTests.java
@@ -0,0 +1,13 @@
+package WebScraper.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScriptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
