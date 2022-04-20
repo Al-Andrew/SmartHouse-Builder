e67633049932b59882702f6136580d59cf30fda2
Aldea Andrei <aldea.andrei.977@gmail.com>
2022-04-20 23:48:03 +0300

    on forum side too

--- BackEnd/Scraping/src/main/java/WebScraper/demo/ScriptApplication.java
+++ BackEnd/Scraping/src/main/java/WebScraper/demo/ScriptApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+
 @SpringBootApplication
 public class ScriptApplication {
 
