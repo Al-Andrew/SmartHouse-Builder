dfe5507b9b7561d58d842ddcda4bbe0b61bed4ee
Aldea Andrei <aldea.andrei.977@gmail.com>
2022-05-01 16:52:20 +0300

    Added a CustomPaint component for the builder

--- FrontEnd/lib/builder.dart
+++ FrontEnd/lib/builder.dart
@@ -1,16 +1,66 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
-class BuilderCon extends StatelessWidget {
-  BuilderCon();
+class BuilderCon extends StatefulWidget {
+  @override
+  BuilderState createState() => BuilderState();
+}
+
+class BuilderState extends State<BuilderCon> {
+  List<Wall> walls = [];
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(8.0),
-      child: Container(
-        color: Colors.deepPurple,
-        child: Center(child: Text("Builderul")),
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext ctx) {
+    return Scaffold(
+      body: CustomPaint(
+        size: MediaQuery.of(ctx).size,
+        painter: BuilderPainter(walls: walls),
+        child: GestureDetector(
+          onTapDown: (details) {
+            walls.add(Wall(
+                p1: details.localPosition + Offset(0, 50),
+                p2: details.localPosition - Offset(0, 50)));
+          },
+        ),
       ),
     );
   }
+}
+
+class Wall {
+  Offset p1, p2;
+  static final Paint paint = Paint()
+    ..strokeCap = StrokeCap.square
+    ..isAntiAlias = true
+    ..color = Colors.black
+    ..strokeWidth = 5;
+
+  Wall({required this.p1, required this.p2});
+}
+
+class BuilderPainter extends CustomPainter {
+  List<Wall> walls;
+
+  BuilderPainter({required this.walls});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint background = Paint()..color = Color.fromARGB(161, 189, 24, 24);
+    Rect rect = Rect.fromLTWH(0, 0, size.width, size.height);
+    canvas.drawRect(rect, background);
+    canvas.clipRect(rect);
+
+    for (Wall w in walls) {
+      canvas.drawLine(w.p1, w.p2, Wall.paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(BuilderPainter oldDelegate) => oldDelegate.walls != walls;
 }
