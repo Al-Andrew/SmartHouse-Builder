0a31e0f0dd9139a0108462245ab2e96f22bf81f5
LapteacruCristian <75312122+LapteacruCristian@users.noreply.github.com>
2022-05-05 01:06:14 +0300

    HOMEPAGE---FORUM table provizoriu+butoanele+searchbar+title

--- FrontEnd/lib/forum/ForumHomePage.dart
+++ FrontEnd/lib/forum/ForumHomePage.dart
@@ -1,6 +1,9 @@
 import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:homepage/forum/GradientText.dart';
+import 'package:homepage/forum/SearchBar.dart';
+import 'package:homepage/forum/PostsTabel.dart';
 
 class Forum extends StatefulWidget {
   Forum();
@@ -17,17 +20,27 @@ class _ForumState extends State<Forum> {
         child: Column(
           children: [
             Expanded(
+              flex: 0,
               child: Container(
-                child: Center(child: Text('Forum')),
-                color: Colors.yellow,
+                child: Center(
+                  child: GradientText(
+                    'Forum',
+                    style: const TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Barlow Condensed',
+                    ),
+                    gradient: LinearGradient(colors: [
+                      Colors.blue.shade400,
+                      Colors.blue.shade900,
+                    ]),
+                  ),
+                ),
               ),
             ),
             Expanded(
               flex: 1,
-              child: Container(
-                child: Center(child: Text('Forum')),
-                color: Colors.purple,
-              ),
+              child: SearchBar(),
             ),
             Expanded(
               flex: 5,
@@ -36,49 +49,79 @@ class _ForumState extends State<Forum> {
                   Expanded(
                     flex: 1,
                     child: Container(
-                        color: Colors.orange,
                         child: Column(
-                          children: [
-                            ButtonBar(
-                                buttonPadding: EdgeInsets.symmetric(
-                                    horizontal: 30, vertical: 10),
-                                children: [
-                                  TextButton(
-                                    style: TextButton.styleFrom(
-                                        textStyle: const TextStyle(
-                                            color: Colors.black)),
-                                    onPressed: () => {
-                                      //do something
-                                    },
-                                    child: const Center(
-                                      child: Text('New Post'),
-                                    ),
-                                  ),
-                                  TextButton(
-                                    style: TextButton.styleFrom(
-                                        textStyle: const TextStyle(
-                                            color: Colors.black)),
-                                    onPressed: () => {
-                                      //do something
-                                    },
-                                    child: const Center(
-                                      child: Text('My Posts'),
-                                    ),
-                                  ),
-                                ]),
-                            Container(
-                              color: Colors.white,
-                              margin: const EdgeInsets.only(top: 10.0),
-                              child: Center(child: Text('Filter')),
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(top: 10.0),
+                          height: 40,
+                          width: 125,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            gradient: LinearGradient(
+                              colors: [
+                                Color.fromARGB(255, 109, 184, 245),
+                                Color.fromARGB(255, 37, 108, 214),
+                              ],
                             ),
-                          ],
-                        )),
+                          ),
+                          child: ElevatedButton.icon(
+                            onPressed: () {},
+                            label: Text(
+                              'New Post',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            icon: Icon(Icons.add),
+                            style: ElevatedButton.styleFrom(
+                                shape: new RoundedRectangleBorder(
+                                  borderRadius: new BorderRadius.circular(20.0),
+                                ),
+                                primary: Colors.transparent,
+                                shadowColor: Colors.transparent),
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(top: 10.0),
+                          height: 40,
+                          width: 125,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            gradient: LinearGradient(
+                              colors: [
+                                Color.fromARGB(255, 109, 184, 245),
+                                Color.fromARGB(255, 37, 108, 214),
+                              ],
+                            ),
+                          ),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                                shape: new RoundedRectangleBorder(
+                                  borderRadius: new BorderRadius.circular(20.0),
+                                ),
+                                primary: Colors.transparent,
+                                shadowColor: Colors.transparent),
+                            child: Text(
+                              'My Posts',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          color: Colors.white,
+                          margin: const EdgeInsets.only(top: 10.0),
+                          child: Center(child: Text('Filter')),
+                        ),
+                      ],
+                    )),
                   ),
                   Expanded(
                     flex: 6,
                     child: Container(
-                      color: Colors.red,
-                      child: Center(child: Text('Forum')),
+                      child: Center(child: PostsTabel()),
                     ),
                   ),
                 ],
