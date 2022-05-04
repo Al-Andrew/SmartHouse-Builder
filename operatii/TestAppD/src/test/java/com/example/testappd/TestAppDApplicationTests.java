7c9ce04dd1e4f62420e63c8b0b0001ae829db745
cristiancibotariu <79245429+cristiancibotariu@users.noreply.github.com>
2022-05-04 22:14:05 +0300

    Rezolvarea issue-ului #58 (#62)
    * Marketplace UI: Prima jumatate a sprint-ului #1
    
    * Clasa Product
    
    * Delete marketplace directory

---
+++ operatii/TestAppD/src/test/java/com/example/testappd/TestAppDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testappd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAppDApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
