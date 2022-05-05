3409d6fcb11782b4977fa496ba69baf6d9e610f7
Balteanu Andrei 2A4 <andreibalteanu2@gmail.com>
2022-05-05 21:01:18 +0300

    Category Rest Api

---
+++ BackEnd/MarketPlace/Categories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package Categories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository  extends JpaRepository<Category,Integer> {
+}
