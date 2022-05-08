033b4482e61038ce936a31b1ad6fcf2ade16f27d
AlexandruBrezuleanu <brezuleanu.mihai.alexandru@gmail.com>
2022-05-08 16:05:00 +0300

    implement comment button

---
+++ BackEnd/Forum/src/main/java/forum/comment/CommentRepository.java
@@ -0,0 +1,13 @@
+package forum.comment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Integer>{
+    @Query("SELECT c FROM Comment c WHERE c.id = ?1")
+    Optional<Comment> findCommentById(int id);
+}
