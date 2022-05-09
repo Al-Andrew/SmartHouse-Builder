c24fdbfb1296bf173c213fbcebfb70700685c7f4
Balteanu Andrei 2A4 <andreibalteanu2@gmail.com>
2022-05-09 20:04:13 +0300

    -

--- BackEnd/MarketPlace/Categories/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package Categories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository  extends JpaRepository<Category,Integer> {
-}
