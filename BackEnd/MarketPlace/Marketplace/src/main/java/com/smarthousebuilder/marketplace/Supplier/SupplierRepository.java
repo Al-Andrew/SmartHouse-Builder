0d0201d10fb6b19fe0df620119ec3602cf7c89ed
OpariucRares <opariucraresioan@gmail.com>
2022-05-09 23:17:23 +0300

    Unified BackEnd! :o

--- BackEnd/MarketPlace/Marketplace/src/main/java/com/smarthousebuilder/marketplace/Supplier/SupplierRepository.java
+++
@@ -1,8 +0,0 @@
-package com.smarthousebuilder.marketplace.Supplier;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SupplierRepository extends JpaRepository<Supplier,Integer> {
-}
--- operatii/TestAppD/src/main/java/Product/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package Product;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository  extends JpaRepository<Product,Integer> {
-}
