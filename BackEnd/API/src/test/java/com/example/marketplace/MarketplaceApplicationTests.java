2faeb96a2fe915234a3845f2f7064366455ff4af
horiaapostol <79245173+horiaapostol@users.noreply.github.com>
2022-05-11 22:31:48 +0300

    Update MarketplaceApplicationTests.java

--- BackEnd/API/src/test/java/com/example/marketplace/MarketplaceApplicationTests.java
+++ BackEnd/API/src/test/java/com/example/marketplace/MarketplaceApplicationTests.java
@@ -3,11 +3,6 @@ package com.example.marketplace;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MarketplaceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
