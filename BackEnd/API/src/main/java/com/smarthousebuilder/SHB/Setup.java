d4832d7c2fed19fea8881d4ab92e88cec803690f
BARANCEANU Vlad-Andrei <79141572+baranceanuvlad@users.noreply.github.com>
2022-05-29 15:29:15 +0300

    Added backend for setups (#204)

---
+++ BackEnd/API/src/main/java/com/smarthousebuilder/SHB/Setup.java
@@ -0,0 +1,90 @@
+package com.smarthousebuilder.SHB;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+
+@Entity
+public class Setup {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Basic
+    @Column(name = "name")
+    private String name;
+    @Basic
+    @Column(name = "user_id")
+    private Integer userId;
+    @Basic
+    @Column(name = "components")
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String components;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getComponents() {
+        return components;
+    }
+
+    public void setComponents(String components) {
+        this.components = components;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Setup setup = (Setup) o;
+
+        if (id != setup.id) return false;
+        if (name != null ? !name.equals(setup.name) : setup.name != null) return false;
+        if (userId != null ? !userId.equals(setup.userId) : setup.userId != null) return false;
+        if (components != null ? !components.equals(setup.components) : setup.components != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (userId != null ? userId.hashCode() : 0);
+        result = 31 * result + (components != null ? components.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Setup{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", userId=" + userId +
+                ", components='" + components + '\'' +
+                '}';
+    }
+}
---
+++ BackEnd/API/src/main/java/com/smarthousebuilder/SHB/SetupService.java
@@ -0,0 +1,33 @@
+package com.smarthousebuilder.SHB;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SetupService {
+    private final SetupRepository setupRepository;
+
+    @Autowired
+    public SetupService(SetupRepository setupRepository) {
+        this.setupRepository = setupRepository;
+    }
+
+    public List<Setup> getAllSetup(){
+        List<Setup> allSetups=setupRepository.findAll();
+        return allSetups;
+    }
+
+    public String getSetupBySetupId(int id){
+        Setup allSetups=setupRepository.findSetupById(id);
+        return allSetups.getComponents();
+    }
+
+    public List<Setup> getSetupByUserId(int id){
+        return setupRepository.findSetupByUserId(id);
+    }
+    public void saveSetup(Setup setup){
+        setupRepository.save(setup);
+    }
+}
