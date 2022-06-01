d5a82c42ca5fc55f7fc5ada755bc8f7b87371339
Balteanu Andrei 2A4 <andreibalteanu2@gmail.com>
2022-06-01 16:56:54 +0300

    get all products by user Id

--- BackEnd/API/src/main/java/com/smarthousebuilder/marketplace/wishlistItem/WishlistItemRepository.java
+++ BackEnd/API/src/main/java/com/smarthousebuilder/marketplace/wishlistItem/WishlistItemRepository.java
@@ -1,15 +1,18 @@
 package com.smarthousebuilder.marketplace.wishlistItem;
 
+import com.smarthousebuilder.marketplace.product.Product;
 import com.smarthousebuilder.marketplace.wishlistItem.WishlistItems;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface WishlistItemRepository extends JpaRepository<WishlistItems,Integer> {
 
     Boolean existsByWishlistIdAndAndProductId(Integer wishlistId, Integer productId);
 
-    WishlistItems findAllByWishlistId(Integer wishlistId);
+    List<WishlistItems> findAllByWishlistId(Integer wishlistId);
 
     Boolean existsByProductIdAndWishlistId(Integer productId, Integer wishlistId);
 
@@ -18,4 +21,5 @@ public interface WishlistItemRepository extends JpaRepository<WishlistItems,Inte
     Integer countAllByWishlistId(Integer wishlistId);
 
     void deleteAllByWishlistId(Integer wishlistId);
+
 }
